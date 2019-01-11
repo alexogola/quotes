5eb27b772fcbd834e6bef5abe63f64c17dba9fc6
Alex Ogola <ogollaokumu@gmail.com>
2019-01-11 08:36:58 +0300

    generate quote-input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { QuoteInputComponent } from './quote-input/quote-input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteInputComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/quote-input/quote-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-input',
+  templateUrl: './quote-input.component.html',
+  styleUrls: ['./quote-input.component.css']
+})
+export class QuoteInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
